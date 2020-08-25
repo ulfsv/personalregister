03ff25a6e95d9505b3d26d2cae38f91ab2415bba
ulfsv <ulf.e.sv@gmail.com>
2020-08-25 16:11:02 +0200

    updated

--- personalregister/Program.cs
+++ personalregister/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
 
-namespace personalregister
+namespace PersonalRegister
 {
     class Program
     {
+
+        public static void AddEmployee(register)
+        {
+            var employee = new Employee();
+            Console.Write("Name: ");
+
+            employee.Name = Console.ReadLine();
+            
+            Console.Write("Salary: ");
+            {
+                employee.Salary = float.Parse(Console.ReadLine());
+            }            
+
+            register.AddEmployee(employee);
+        }
+
+
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            Register register = new Register();
+            int menu = 0;          
+
+
+            }
+
+            Console.WriteLine("Program exit");
         }
     }
 }
