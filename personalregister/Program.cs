d78815bffe0d854090d0c01abcdce7b152b8ce2f
ulfsv <ulf.e.sv@gmail.com>
2020-08-25 14:57:12 +0200

    files added

---
+++ personalregister/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace personalregister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
